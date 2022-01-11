79ec8dafb8d08c704def6512c7b67a666a1c8e27
Sachini <sachinideepasika.97@gmail.com>
2022-01-11 15:15:24 +0530

    modifying python evaluation

--- lib/datasets/nih.py
+++ lib/datasets/nih.py
@@ -370,27 +370,26 @@ class nih(imdb):
                                        dets[k, 2] + 1, dets[k, 3] + 1))
 
     def _do_python_eval(self, output_dir='output'):
+
         annopath = os.path.join(
-            self._devkit_path,
-            'Annotations',
-            '{:s}.xml')
+            self._data_path,
+            'Annotations', 'BBox_List_2017' + '.csv')
         imagesetfile = os.path.join(
-            self._devkit_path,
-            'ImageSets',
+            self._data_path, 'ImageSets',
             self._image_set + '.txt')
         cachedir = os.path.join(self._devkit_path, 'annotations_cache')
         aps = []
-        # The PASCAL VOC metric changed in 2010
-        # use_07_metric = True if int(self._year) < 2010 else False
-        # print('VOC07 metric? ' + ('Yes' if use_07_metric else 'No'))
+
         if not os.path.isdir(output_dir):
             os.mkdir(output_dir)
+
         for i, cls in enumerate(self._classes):
             if cls == '__background__':
                 continue
             filename = self._get_nih_results_file_template().format(cls)
             rec, prec, ap = nih_eval(
                 filename, annopath, imagesetfile, cls, cachedir, ovthresh=0.5)
+
             aps += [ap]
             print('AP for {} = {:.4f}'.format(cls, ap))
             with open(os.path.join(output_dir, cls + '_pr.pkl'), 'wb') as f:
@@ -429,6 +428,7 @@ class nih(imdb):
         self._write_nih_results_file(all_boxes)
         self._do_python_eval(output_dir)
         if self.config['matlab_eval']:
+            print ('doing mathlab eval')
             self._do_matlab_eval(output_dir)
         if self.config['cleanup']:
             for cls in self._classes:
